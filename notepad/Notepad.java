6e873f4a429e6e7c55df94c96896a15001ea1dd9
Minanshu-Dhiman <152035106+Minanshu-Dhiman@users.noreply.github.com>
2024-10-17 01:04:56 +0530

    added shortcuts

---
+++ notepad/Notepad.java
@@ -0,0 +1,143 @@
+package notepad;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.NoRouteToHostException;
+
+public class Notepad extends JFrame implements ActionListener {
+    JTextArea area;
+    Notepad() {
+        setTitle("Notepad");
+        ImageIcon notepadIcon = new ImageIcon(ClassLoader.getSystemResource("notepad/icons/notepad.png"));
+        Image icon = notepadIcon.getImage();
+        setIconImage(icon);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBackground(Color.white);
+
+
+        JMenu file = new JMenu("File");
+        file.setFont(new Font("AERIAL", Font.PLAIN, 14));
+        
+        JMenuItem newDoc = new JMenuItem("New");
+        newDoc.addActionListener(this);
+        newDoc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        file.add(newDoc);
+
+        JMenuItem open = new JMenuItem("Open");
+        open.addActionListener(this);
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+        file.add(open);
+
+        JMenuItem save = new JMenuItem("Save");
+        save.addActionListener(this);
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        file.add(save);
+
+        JMenuItem print = new JMenuItem("Print");
+        print.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+        file.add(print);
+
+        JMenuItem exit = new JMenuItem("Exit");
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, ActionEvent.CTRL_MASK));
+        file.add(exit);
+
+        menuBar.add(file);
+
+        JMenu edit = new JMenu("Edit");
+        edit.setFont(new Font("AERIAL", Font.PLAIN, 14));
+
+        JMenuItem copy = new JMenuItem("Copy");
+        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        edit.add(copy);
+
+        JMenuItem paste = new JMenuItem("Paste");
+        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
+        edit.add(paste);
+
+        JMenuItem cut = new JMenuItem("Cut");
+        cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
+        edit.add(cut);
+
+        JMenuItem selectAll = new JMenuItem("Select All");
+        selectAll.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+        edit.add(selectAll);
+
+        menuBar.add(edit);
+
+        JMenu helpMenu = new JMenu("Help");
+        helpMenu.setFont(new Font("AERIAL", Font.PLAIN, 14));
+
+        JMenuItem help = new JMenuItem("Help");
+        help.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.CTRL_MASK));
+        helpMenu.add(help);
+
+        menuBar.add(helpMenu);
+
+        setJMenuBar(menuBar);
+
+        area = new JTextArea();
+        area.setFont(new Font("SAN_SERIF",Font.PLAIN, 18));
+        area.setLineWrap(true);
+        area.setWrapStyleWord(true);
+        add(area);
+
+        JScrollPane pane = new JScrollPane(area);
+        pane.setBorder(BorderFactory.createEmptyBorder());
+        add(pane);
+
+
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getActionCommand().equals("New"))
+            area.setText("");
+        else if (actionEvent.getActionCommand().equals("Open")) {
+            JFileChooser chooser = new JFileChooser("D:/Java");
+            chooser.setAcceptAllFileFilterUsed(false);
+            FileNameExtensionFilter restrict = new FileNameExtensionFilter("Only .txt files", "txt");
+            chooser.addChoosableFileFilter(restrict);
+
+            int action = chooser.showOpenDialog(this);
+            if (action != JFileChooser.APPROVE_OPTION) {
+                return;
+            }
+            File file = chooser.getSelectedFile();
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                area.read(reader, actionEvent);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else if (actionEvent.getActionCommand().equals("Save")) {
+            JFileChooser saveas = new JFileChooser();
+            saveas.setApproveButtonText("Save");
+
+            int action = saveas.showOpenDialog(this);
+
+            if (action != JFileChooser.APPROVE_OPTION) {
+                return;
+            }
+            File fileName = new File(saveas.getSelectedFile() + ".txt");
+            BufferedWriter outFile = null;
+            try {
+                outFile = new BufferedWriter(new FileWriter(fileName));
+                area.write(outFile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Notepad();
+    }
+}
